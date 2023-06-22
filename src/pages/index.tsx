fa400968dd69ec9f26106c8d1ff94ae8f05be412
Dante Vicenzo <dantevcz@gmail.com>
2023-06-22 02:24:56 -0300

    Fetching stripe api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,9 @@ import { Handbag } from '@phosphor-icons/react'
 import { KeenSliderPlugin, useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
-import camiseta1 from '../assets/camisetas/camiseta1.png'
-import camiseta2 from '../assets/camisetas/camiseta2.png'
-import camiseta3 from '../assets/camisetas/camiseta3.png'
-import camiseta4 from '../assets/camisetas/camiseta4.png'
-import camiseta5 from '../assets/camisetas/camiseta5.png'
+import { stripe } from '@/lib/stripe'
+import Stripe from 'stripe'
+import { GetStaticProps } from 'next'
 
 const WheelControls: KeenSliderPlugin = (slider) => {
   let touchTimeout: ReturnType<typeof setTimeout>
@@ -74,7 +72,19 @@ const WheelControls: KeenSliderPlugin = (slider) => {
   })
 }
 
-export default function Home() {
+interface IProduct {
+  id: string
+  name: string
+  price: number
+  description: string
+  imageUrl: string
+}
+
+interface IHomeProps {
+  products: IProduct[]
+}
+
+export default function Home(props: IHomeProps) {
   const [ref] = useKeenSlider<HTMLDivElement>(
     {
       loop: false,
@@ -89,70 +99,49 @@ export default function Home() {
 
   return (
     <Container ref={ref} className="keen-slider">
-      <Card className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
-        <CardDetails>
-          <div>
-            <CartTitle>Camiseta Beyond the Limits</CartTitle>
-            <CartPrice>R$ 79,90</CartPrice>
-          </div>
-          <AddToCartButton type="button">
-            <Handbag size={32} />
-          </AddToCartButton>
-        </CardDetails>
-      </Card>
-
-      <Card className="keen-slider__slide">
-        <Image src={camiseta2} width={520} height={480} alt="" />
-        <CardDetails>
-          <div>
-            <CartTitle>Camiseta Beyond the Limits</CartTitle>
-            <CartPrice>R$ 79,90</CartPrice>
-          </div>
-          <AddToCartButton type="button">
-            <Handbag size={32} />
-          </AddToCartButton>
-        </CardDetails>
-      </Card>
-
-      <Card className="keen-slider__slide">
-        <Image src={camiseta3} width={520} height={480} alt="" />
-        <CardDetails>
-          <div>
-            <CartTitle>Camiseta Beyond the Limits</CartTitle>
-            <CartPrice>R$ 79,90</CartPrice>
-          </div>
-          <AddToCartButton type="button">
-            <Handbag size={32} />
-          </AddToCartButton>
-        </CardDetails>
-      </Card>
-
-      <Card className="keen-slider__slide">
-        <Image src={camiseta4} width={520} height={480} alt="" />
-        <CardDetails>
-          <div>
-            <CartTitle>Camiseta Beyond the Limits</CartTitle>
-            <CartPrice>R$ 79,90</CartPrice>
-          </div>
-          <AddToCartButton type="button">
-            <Handbag size={32} />
-          </AddToCartButton>
-        </CardDetails>
-      </Card>
-
-      <Card className="keen-slider__slide">
-        <Image src={camiseta5} width={520} height={480} alt="" />
-        <CardDetails>
-          <div>
-            <CartTitle>Camiseta Beyond the Limits</CartTitle>
-            <CartPrice>R$ 79,90</CartPrice>
-          </div>
-          <AddToCartButton type="button">
-            <Handbag size={32} />
-          </AddToCartButton>
-        </CardDetails>
-      </Card>
+      {props.products.map((product) => (
+        <Card key={product.id} className="keen-slider__slide">
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <CardDetails>
+            <div>
+              <CartTitle>{product.name}</CartTitle>
+              <CartPrice>{product.price}</CartPrice>
+            </div>
+            <AddToCartButton type="button">
+              <Handbag size={32} />
+            </AddToCartButton>
+          </CardDetails>
+        </Card>
+      ))}
     </Container>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = res.data.map((product) => {
+    const stripePrice = product.default_price as Stripe.Price
+    const priceInCents = stripePrice.unit_amount as number
+
+    return {
+      id: product.id,
+      name: product.name,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(priceInCents / 100),
+      description: product.description,
+      imageUrl: product.images[0],
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  }
 }
