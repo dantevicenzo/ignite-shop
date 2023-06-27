a49c2a9f3e94d60cae56134f5cd4cfc48756b189
Dante Vicenzo <dantevcz@gmail.com>
2023-06-26 21:52:21 -0300

    Change api checkout

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,10 +5,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed.' })
-  }
-
   const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
   const { priceId } = req.body
